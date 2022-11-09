9d8b4ab1f3a3d4e86e6816d4cd124438427e3e2a
TarekZERGAT <t.zergat@gmail.com>
2022-11-09 18:46:51 +0100

    Creation du projet API

---
+++ ztar-admin-api/src/test/java/com/ztardz/ztaradminapi/ZtarAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztardz.ztaradminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtarAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
