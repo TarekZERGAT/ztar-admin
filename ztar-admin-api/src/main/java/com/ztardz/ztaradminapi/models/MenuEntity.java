776685588c2fa7586df6bda3d8e8e476d3e01146
TarekZERGAT <t.zergat@gmail.com>
2022-11-10 16:00:04 +0100

    Creation de l'Entity Menu

---
+++ ztar-admin-api/src/main/java/com/ztardz/ztaradminapi/models/MenuEntity.java
@@ -0,0 +1,19 @@
+package com.ztardz.ztaradminapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sessions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MenuEntity extends AbstractEntity{
+    @Column(name = "name", length=50, nullable=false, unique = true)
+    private String name;
+}
