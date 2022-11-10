d56aed2e9c361fca8a0d9dd08f248f71cccdca14
TarekZERGAT <t.zergat@gmail.com>
2022-11-10 15:24:29 +0100

    AbstractEntity Implements Serialisable

--- ztar-admin-api/src/main/java/com/ztardz/ztaradminapi/models/AbstractEntity.java
+++ ztar-admin-api/src/main/java/com/ztardz/ztaradminapi/models/AbstractEntity.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
@@ -13,7 +14,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public abstract class AbstractEntity {
+public abstract class AbstractEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id")
